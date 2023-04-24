03cc4cb3c9b1d5bdcbc39549c8b6d58fef07ea11
felipcaffe <114303439+felipcaffe@users.noreply.github.com>
2023-04-24 12:39:24 -0300

    Arreglo Test

--- src/test/java/PartidosTest.java
+++ src/test/java/PartidosTest.java
@@ -1,39 +1,81 @@
+import ArgentinaPrograma_TP_Final.modelo.Equipo;
+import ArgentinaPrograma_TP_Final.modelo.Partido;
+import ArgentinaPrograma_TP_Final.modelo.ResultadoEnum;
+import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 
-public class PartidosTest {
-/*    @Test
+public class PartidoTest {
+    private Equipo equipo1;
+    private Equipo equipo2;
+    private Partido partido;
+
+
+    //GANA 2
+    @Test
+    public void testPartido1() {
+        this.equipo1 = new Equipo("Arg", "");
+        this.equipo2 = new Equipo("Br", "");
+        this.partido = new Partido(this.equipo1, this.equipo2, 2, 3);
+        assertEquals(this.partido.getResultado(), ResultadoEnum.GANA2);
+    }
+
+    //GANA 1
+    @Test
+    public void testPartido2() {
+        this.equipo1 = new Equipo("Arg", "");
+        this.equipo2 = new Equipo("Br", "");
+        this.partido = new Partido(this.equipo1, this.equipo2, 5, 1);
+        assertEquals(this.partido.getResultado(), ResultadoEnum.GANA1);
+    }
+
+    //EMPATE
+    @Test
+    public void testPartido3() {
+        this.equipo1 = new Equipo("Arg", "");
+        this.equipo2 = new Equipo("Br", "");
+        this.partido = new Partido(this.equipo1, this.equipo2, 3, 3);
+        assertEquals(this.partido.getResultado(), ResultadoEnum.EMPATE);
+    }
+
+
+
+
+
+    //Test anteriores arreglados
+    @Test
     public void probarResultadoPerdedor1(){
         Equipo equipo1= new Equipo("Pais 1", "1");
         Equipo equipo2= new Equipo("Pais 2", "2");
         Partido partido = new Partido(equipo1, equipo2, 1,2 );
-        assertEquals (ResultadoEnum.PERDEDOR, partido.Resultado(equipo1));
+        assertEquals (ResultadoEnum.GANA2, partido.getResultado());
     }
     @Test
     public void probarResultadoPerdedor2(){
         Equipo equipo1= new Equipo("Pais 1", "1");
         Equipo equipo2= new Equipo("Pais 2", "2");
         Partido partido = new Partido(equipo1, equipo2, 2,1 );
-        assertEquals (ResultadoEnum.PERDEDOR, partido.Resultado(equipo2));
+        assertEquals (ResultadoEnum.GANA1, partido.getResultado());
     }
     @Test
     public void probarResultadoGanador1(){
         Equipo equipo1= new Equipo("Pais 1", "1");
         Equipo equipo2= new Equipo("Pais 2", "2");
         Partido partido = new Partido(equipo1, equipo2, 2,1 );
-        assertEquals (ResultadoEnum.GANADOR, partido.Resultado(equipo1));
+        assertEquals (ResultadoEnum.GANA1, partido.getResultado());
     }
     @Test
     public void probarResultadoGanador2(){
         Equipo equipo1= new Equipo("Pais 1", "1");
         Equipo equipo2= new Equipo("Pais 2", "2");
         Partido partido = new Partido(equipo1, equipo2, 1,2 );
-        assertEquals (ResultadoEnum.GANADOR, partido.Resultado(equipo2));
+        assertEquals (ResultadoEnum.GANA2, partido.getResultado());
     }
     @Test
     public void probarResultadoEmpate(){
         Equipo equipo1= new Equipo("Pais 1", "1");
         Equipo equipo2= new Equipo("Pais 2", "2");
         Partido partido = new Partido(equipo1, equipo2, 2,2 );
-        assertEquals (ResultadoEnum.EMPATE, partido.Resultado(equipo1));
-    }*/
+        assertEquals (ResultadoEnum.EMPATE, partido.getResultado());
+    }
 }
