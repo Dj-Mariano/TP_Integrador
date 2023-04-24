2c2bff3ee5b30a755866eea4fc813e501e67e7d9
felipcaffe <114303439+felipcaffe@users.noreply.github.com>
2023-04-24 17:43:04 -0300

    Nuevo Test TodosLosAciertos

--- src/test/java/PuntosPersonasTest.java
+++ src/test/java/PuntosPersonasTest.java
@@ -18,6 +18,11 @@ public class PuntosPersonasTest {
     Pronostico pronosticoP2;
     Ronda ronda;
     Fase fase;
+
+    Ronda ronda2;
+    Partido partido2;
+    Pronostico pronostico2P1;
+    Pronostico pronostico2P2;
     List<Pronostico> pronosticos = new ArrayList<>();
 
     private void calcularPuntosTest(List<Pronostico> pronosticos, int puntosAcierto) {
@@ -30,6 +35,15 @@ public class PuntosPersonasTest {
         }
     }
 
+
+    public boolean acertoTodosPronosticosFasesTest(Persona p, int fase){
+
+        List<Pronostico> pronosticosFiltrado = this.pronosticos.stream().filter(pronostico -> pronostico.getPersona().equals(p)).toList();
+
+        pronosticosFiltrado = pronosticosFiltrado.stream().filter(pronostico -> pronostico.getFase().getNumeroFase() == fase).toList();
+
+        return pronosticosFiltrado.stream().allMatch(pronostico -> pronostico.pronosticoAcertado());
+    }
 
     @Test
     public void ProbarSumaDePuntosYAciertos(){
@@ -50,5 +64,36 @@ public class PuntosPersonasTest {
         calcularPuntosTest(pronosticos, 1);
 
         assertTrue(persona1.getPuntos()> persona2.getPuntos() && persona1.getCantAciertos()> persona2.getCantAciertos());
+    }
+
+    @Test
+    public void ProbarTodosLosAciertos(){
+        fase= new Fase(1);
+
+        ronda= new Ronda(1, fase.getNumeroFase());
+        ronda2= new Ronda(2, fase.getNumeroFase());
+
+        partido1  = new Partido(equipo1,equipo2,0,1);
+        partido2  = new Partido(equipo1,equipo2,4,2);
+
+        persona1= new Persona();
+        persona1.setNombre("Jesus");
+        persona2= new Persona();
+        persona2.setNombre("Alejo");
+
+        equipo1=new Equipo("Chile", "1");
+        equipo2=new Equipo("Uruguay", "2");
+
+        pronosticoP1 = new Pronostico(fase, ronda, persona1, partido1, ResultadoEnum.GANA2);
+        pronosticoP2 = new Pronostico(fase, ronda, persona2, partido1, ResultadoEnum.EMPATE);
+        pronostico2P1 = new Pronostico(fase, ronda2, persona1, partido2, ResultadoEnum.GANA1);
+        pronostico2P2 = new Pronostico(fase, ronda2, persona2, partido2, ResultadoEnum.GANA2);
+
+        pronosticos.add(pronosticoP1);
+        pronosticos.add(pronosticoP2);
+        pronosticos.add(pronostico2P1);
+        pronosticos.add(pronostico2P2);
+
+        assertTrue(acertoTodosPronosticosFasesTest(persona1, 1));
     }
 }
